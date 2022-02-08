de009645d82c708a98b0b78e8979c1b63fd74f9f
Onyedikachi Nze-Ben <nzebenflorence@gmail.com>
2022-02-08 01:17:06 +0100

    setup: basic setup done-WIP

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+
+    colors: {
+      primary: {
+        DEFAULT: "#6941C6",
+        light: "#F9F5FF",
+      },
+      success: "#027A48",
+      error: "#B42318",
+      "gray-500": "#667085",
+      "gray-700": "#344054",
+      "gray-900": "#101828",
+    },
+
+    fontSize: {
+      xs: "12px",
+      sm: "14px",
+      base: "16px",
+      lg: "24px",
+      xl: "30px",
+    },
+
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  },
+};
